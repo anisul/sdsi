52bd1c16d23b79dd924bdb6b93371ced1c60fbdd
anisul <anis.2803@gmail.com>
2018-10-30 23:27:47 +0100

    cleanup

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,10 +1,6 @@
 import com.hazelcast.core.Hazelcast;
 import util.AppUtil;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class Member {
 
     public static void main(String[] args) {
