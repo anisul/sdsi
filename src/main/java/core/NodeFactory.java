651149b61bb657efe3631f025a1e4317a9543d47
anisul <anis.2803@gmail.com>
2018-10-31 18:08:32 +0100

    add search and store mechanism

--- src/main/java/core/NodeFactory.java
+++ src/main/java/core/NodeFactory.java
@@ -1,5 +1,6 @@
 package core;
 
+import types.Bin;
 import types.Node;
 import util.AppUtil;
 
@@ -7,13 +8,79 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NodeFactory {
-    private List<Node> nodes = new ArrayList<Node>();
+    private static List<Node> nodes = new ArrayList<Node>();
 
+    /*
+    * Initializes nodes with bins (bins have random addresses while being generated)
+    * */
     public void initialize() {
         this.nodes.clear();
         for (int i = 0; i < AppUtil.peerCount; i++) {
             Node n = new Node(i);
             nodes.add(n);
         }
+    }
+
+    /*
+    * Store data in bins comparing the hamming distance.
+    */
+    public void store(int[] input) {
+        for (Node node : nodes) {
+            for (Bin bin : node.getBins()) {
+                if (AppUtil.calculateHammingDistance(bin.getAddress(), input) < AppUtil.hammingThreshold) {
+                    bin.setData(AppUtil.sumTwoEntryBitwise(bin.getData(), AppUtil.bipolarConversion(input)) );
+                }
+            }
+        }
+    }
+
+    /**
+     * Searches inside all the bins in all the nodes
+     * @param input searching data
+     * @return searched address
+     */
+    public int[] search(int[] input) {
+        int[] result = new int[input.length];
+
+        for (int i = 0; i < nodes.size(); i++) {
+            for (int j = 0; j < nodes.get(i).getBins().size(); j++) {
+                if (AppUtil.calculateHammingDistance(nodes.get(i).getBins().get(j).getAddress(), input) < AppUtil.hammingThreshold) {
+                    result = AppUtil.sumTwoEntryBitwise(result, nodes.get(i).getBins().get(i).getAddress());
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Iteratively searches inside all the bins in all the nodes
+     * @param input searching data
+     * @return reverse bi-polarized search result
+     */
+    public int[] searchWrapper(int[] input) {
+        int[] result;
+        result = search(input);
+
+        do {
+            result = search(result);
+            if (AppUtil.calculateHammingDistance(input, result) == 0) {
+                break;
+            } else if (AppUtil.calculateHammingDistance(input, result) < 0
+                    || AppUtil.calculateHammingDistance(input, result) == AppUtil.searchConvergenceUpperBound) {
+                System.out.println("Search diverges!");
+                break;
+            }
+        } while (AppUtil.calculateHammingDistance(input, result) > 0
+                && AppUtil.calculateHammingDistance(input, result) < AppUtil.searchConvergenceUpperBound);
+
+        return AppUtil.reverseBipolarConversion(result);
+    }
+
+    /**
+     * Returns list of nodes in member
+     * @return searched addresses
+     */
+    public static List<Node> getNodes() {
+        return nodes;
     }
 }
--- src/main/java/util/AppUtil.java
+++ src/main/java/util/AppUtil.java
@@ -15,17 +15,27 @@ public class AppUtil {
     public static int peerCount;
     public static int hammingThreshold;
     public static int binaryConversionThreshold;
+    public static int searchConvergenceUpperBound;
 
     public static HazelcastInstance hazelcastInstance;
 
+    /**
+    * Load properties from the app.properties file
+    */
     public static void loadProperties(){
         lengthOfData = Integer.parseInt(rb.getString("node.data.length"));
         chunkCount = Integer.parseInt(rb.getString("node.chunk.count"));
         peerCount = Integer.parseInt(rb.getString("member.peer.count"));
         hammingThreshold = Integer.parseInt(rb.getString("core.hamming.threshold"));
         binaryConversionThreshold = Integer.parseInt(rb.getString("core.binary.conversion.threshold"));
+        searchConvergenceUpperBound = lengthOfData/2;
     }
 
+    /**
+    * Generates random binary address of given length
+    * @param lengthOfAddress length of the address
+    * @return random binary address of given length
+    */
     public static int[] generateBinaryRandomAddress(int lengthOfAddress) {
         int[] data = new int[lengthOfAddress];
         Random r = new Random();
@@ -36,6 +46,12 @@ public class AppUtil {
         return data;
     }
 
+    /**
+     * calculates the hamming distance
+     * @param data1 one of two inputs to calculate hamming distance
+     * @param data2 one of two inputs to calculate hamming distance
+     * @return hamming distance
+     */
     public static int calculateHammingDistance(int[] data1, int[] data2) {
         int output = 0;
         for (int i = 0; i < data1.length; i++) {
@@ -46,6 +62,11 @@ public class AppUtil {
         return output;
     }
 
+    /**
+     * bipolar conversion
+     * @param input address
+     * @return bi-polarized address
+     */
     public static int[] bipolarConversion(int[] input) {
         for (int i = 0; i < input.length; i++) {
             if (input[i] == 0) {
@@ -55,7 +76,23 @@ public class AppUtil {
         return input;
     }
 
-    public static int[] sumBitwise(int[][] input, int lengthOfData) {
+    /**
+     * reverse bipolar conversion
+     * @param input address
+     * @return reverse-bi-polarized address
+     */
+    public static int[] reverseBipolarConversion(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] >= 0) {
+                input[i] = 1;
+            } else {
+                input[i] = 0;
+            }
+        }
+        return input;
+    }
+
+    public static int[] sumMemberResultBitwise(int[][] input, int lengthOfData) {
         int[] output = new int[lengthOfData];
         for (int i = 0; i < lengthOfData; i++) {
             int sum = 0;
@@ -68,5 +105,24 @@ public class AppUtil {
             output[i] = sum;
         }
         return output;
+    }
+
+    /**
+     * Generates random binary address of given length
+     * @param input1 address1
+     * @param  input2 address2
+     * @return bitwise summation of two addresses
+     */
+    public static int[] sumTwoEntryBitwise(int[] input1, int[] input2) {
+        int[] result = new int[input1.length];
+
+        int sum;
+        for (int i = 0; i < input1.length; i++) {
+            sum = input1[i] + input2[i];
+            if (sum < AppUtil.binaryConversionThreshold) {
+                result[i] = sum;
+            }
+        }
+        return result;
     }
 }
